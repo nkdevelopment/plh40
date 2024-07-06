d274401ac1e41d885844939411420282fefe692b
Nektarios <nekdevelopment@gmail.com>
2024-07-06 15:19:42 +0300

    Create delete-channel-presscurr.js

---
+++ Website Code/delete-channel-presscurr.js
@@ -0,0 +1,21 @@
+
+document.getElementById("btn_clear_channel").onclick = function() {myFunction()};
+
+function myFunction() {
+	
+	if (confirm("Are you sure?") == true) {
+	
+	fetch('https://api.thingspeak.com/channels/2277637/feeds.json?api_key=8SS6HMRMLNAGW0BF',{
+            method:"DELETE"
+        })
+        .then(response => response.json())
+        .then(data => console.log(data))
+		
+		
+		location.reload();
+		
+	  } else {
+	  }
+	  
+
+}
